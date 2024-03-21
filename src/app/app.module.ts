1a2865c2e47463ac5e6e496b6ae047415dcda428
Satyam <satyamdhamne822@gmail.com>
2024-03-21 19:05:25 +0530

    card changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginCardComponent } from './login-card/login-card.component';
+import  { FormsModule, ReactiveFormsModule } from  '@angular/forms' ;
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/login-card/login-card.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+/**
+ * @title Card with sub-title
+ */
+@Component({
+  selector: 'app-login-card',
+  templateUrl: './login-card.component.html',
+  styleUrls: ['./login-card.component.scss']
+})
+export class LoginCardComponent {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  hide = true;
+}
+
